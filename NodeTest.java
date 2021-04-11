d22b59f16f75f1a9667fd7f96136e8d09dd7eda2
Sanketh1220 <sanketh.babbur11@gmail.com>
2021-04-11 21:51:53 +0530

    ADD: inserted a number in between and test case

--- NodeTest.java
+++ NodeTest.java
@@ -49,4 +49,19 @@ public class NodeTest {
                 linkedList.tail.equals(thirdNode);
         Assertions.assertTrue(res);
     }
+
+    @Test
+    public void Given3Numbers_WhenInserted_ToLinkedListShould_BeAddedInBetween() {
+        Node<Integer> firstNode = new Node<Integer>(56);
+        Node<Integer> secondNode = new Node<Integer>(30);
+        Node<Integer> thirdNode = new Node<Integer>(70);
+        NewLinkList linkedList = new NewLinkList();
+        linkedList.add(firstNode);
+        linkedList.append(thirdNode);
+        linkedList.insert(firstNode, secondNode);
+        boolean res = linkedList.head.equals(firstNode) &&
+                linkedList.head.getNext().equals(secondNode) &&
+                linkedList.tail.equals(thirdNode);
+        Assertions.assertTrue(res);
+    }
 }
