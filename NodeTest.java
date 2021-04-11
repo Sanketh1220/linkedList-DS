f8045d7aa42d880b412d35709c99d95598155602
Sanketh1220 <sanketh.babbur11@gmail.com>
2021-04-11 21:38:09 +0530

    ADD: append method and test case

--- NodeTest.java
+++ NodeTest.java
@@ -33,4 +33,20 @@ public class NodeTest {
                 linkedList.tail.equals(firstNode);
         Assertions.assertTrue(res);
     }
+
+    @Test
+    public void Given3Numbers_WhenAppended_ToLinkedListShould_BeAddedToLast() {
+        Node<Integer> firstNode = new Node<Integer>(56);
+        Node<Integer> secondNode = new Node<Integer>(30);
+        Node<Integer> thirdNode = new Node<Integer>(70);
+        NewLinkList linkedList = new NewLinkList();
+        linkedList.add(firstNode);
+        linkedList.append(secondNode);
+        linkedList.append(thirdNode);
+        linkedList.printNodes();
+        boolean res = linkedList.head.equals(firstNode) &&
+                linkedList.head.getNext().equals(secondNode) &&
+                linkedList.tail.equals(thirdNode);
+        Assertions.assertTrue(res);
+    }
 }
